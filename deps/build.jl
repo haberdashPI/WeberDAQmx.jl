2b4c7e5c783295dcf90e5c3ea6dbaeef5a5cae27
David Little <david.frank.little@gmail.com>
2017-03-12 22:43:59 -0700

    Added build script to add needed python library.

---
+++ deps/build.jl
@@ -0,0 +1,6 @@
+using Conda
+#===============================================================================
+NOTE: PyDAQmx is a conda package I built from a PyPI package,
+following the directions at https://conda.io/docs/build_tutorials/pkgs.html.
+===============================================================================#
+Conda.add_channel("haberdashPI"); Conda.add("PyDAQmx")
